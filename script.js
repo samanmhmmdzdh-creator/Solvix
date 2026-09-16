5d3dbe3a47f6bc195bf7f79b7869caf1efca820a
samanmhmmdzdh-creator <samanmhmmdzdh@gmail.com>
2026-09-16 21:53:51 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
+function calculateMolarity() {
 
+    const mass = parseFloat(document.getElementById("mass").value);
+    const molecularWeight = parseFloat(
+        document.getElementById("molecularWeight").value
+    );
+    const volume =
