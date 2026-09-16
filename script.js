5f6fb71217d4b0dca3e8f2b168a974074afe6cc7
samanmhmmdzdh-creator <samanmhmmdzdh@gmail.com>
2026-09-16 21:54:56 +0330

    Update script.js

--- script.js
+++ script.js
@@ -4,4 +4,23 @@ function calculateMolarity() {
     const molecularWeight = parseFloat(
         document.getElementById("molecularWeight").value
     );
-    const volume =
+    const volume = parseFloat(document.getElementById("volume").value);
+
+    const result = document.getElementById("result");
+
+    if (
+        isNaN(mass) ||
+        isNaN(molecularWeight) ||
+        isNaN(volume) ||
+        mass <= 0 ||
+        molecularWeight <= 0 ||
+        volume <= 0
+    ) {
+        result.textContent = "Please enter valid values.";
+        return;
+    }
+
+    const molarity = mass / molecularWeight / volume;
+
+    result.textContent = `Molarity = ${molarity.toFixed(3)} M`;
+}
